dfcd5df76a3107373e5463d8c3c9382cf160b19b
hershyYJ <juj0478@naver.com>
2025-04-05 16:49:56 +0900

    [feature/#24] Add mapper for mapping member entity to StudyGroupMemberDto

--- src/main/java/com/api/meetudy/study/group/mapper/StudyGroupMapper.java
+++ src/main/java/com/api/meetudy/study/group/mapper/StudyGroupMapper.java
@@ -1,13 +1,17 @@
 package com.api.meetudy.study.group.mapper;
 
+import com.api.meetudy.interest.entity.Interest;
+import com.api.meetudy.interest.entity.MemberInterest;
 import com.api.meetudy.member.entity.Member;
-import com.api.meetudy.study.group.dto.StudyGroupApplicantDto;
 import com.api.meetudy.study.group.dto.StudyGroupDto;
+import com.api.meetudy.study.group.dto.StudyGroupMemberDto;
 import com.api.meetudy.study.group.dto.StudyGroupUpdateDto;
 import com.api.meetudy.study.group.entity.StudyGroup;
 import com.api.meetudy.study.group.entity.StudyGroupMember;
+import com.api.meetudy.study.group.enums.StudyCategory;
 import org.mapstruct.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,23 +31,32 @@ public interface StudyGroupMapper {
     @Mapping(source = "member.major", target = "major")
     @Mapping(source = "member.introduction", target = "introduction")
     @Mapping(source = "member.activityScore", target = "activityScore")
-    StudyGroupApplicantDto toStudyGroupApplicantDto(StudyGroupMember studyGroupMember);
+    @Mapping(source = "member", target = "interests")
+    StudyGroupMemberDto toStudyGroupMemberDto(StudyGroupMember studyGroupMember);
 
     StudyGroupDto toStudyGroupDto(StudyGroup studyGroup);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateStudyGroupFromDto(StudyGroupUpdateDto dto, @MappingTarget StudyGroup studyGroup);
 
-    @IterableMapping(elementTargetType = StudyGroupApplicantDto.class)
-    List<StudyGroupApplicantDto> toStudyGroupApplicantDtoList(List<StudyGroupMember> studyGroupMembers);
-
     @IterableMapping(elementTargetType = StudyGroupDto.class)
     List<StudyGroupDto> toStudyGroupDtoList(List<StudyGroup> studyGroups);
+
+    @IterableMapping(elementTargetType = StudyGroupMemberDto.class)
+    List<StudyGroupMemberDto> toStudyGroupMemberDtoList(List<StudyGroupMember> studyGroupMembers);
 
     default List<StudyGroupDto> toStudyGroupDtoListFromMembers(List<StudyGroupMember> studyGroupMembers) {
         return studyGroupMembers.stream()
                 .map(StudyGroupMember::getStudyGroup)
                 .map(this::toStudyGroupDto)
+                .collect(Collectors.toList());
+    }
+
+    default List<StudyCategory> mapInterests(Member member) {
+        if (member.getMemberInterests() == null) return Collections.emptyList();
+        return member.getMemberInterests().stream()
+                .map(MemberInterest::getInterest)
+                .map(Interest::getStudyCategory)
                 .collect(Collectors.toList());
     }
 
