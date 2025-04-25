19d6aa52b627c686114ae37ffa6cf6256636f29a
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-04-25 15:40:41 +0900

    [chore/#30] Remove unnecessary dependencies

--- src/main/java/com/api/meetudy/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/api/meetudy/chat/repository/ChatRoomRepository.java
@@ -4,9 +4,6 @@ import com.api.meetudy.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
