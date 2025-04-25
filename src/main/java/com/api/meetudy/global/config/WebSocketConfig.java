1076187252989855eb0ea0f69ce8870120642ef3
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-04-25 15:28:32 +0900

    [feature/#30] Implement WebSocketConfig to configure STOMP connection

---
+++ src/main/java/com/api/meetudy/global/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.api.meetudy.global.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
+    }
+
+}
