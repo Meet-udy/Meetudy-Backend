0011a56d091551165272a6bf16a798a8430cc281
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-05-06 15:55:07 +0900

    [chore/#32] Add @JsonBackReference annotation to member

--- src/main/java/com/api/meetudy/chat/entity/ChatRoomMember.java
+++ src/main/java/com/api/meetudy/chat/entity/ChatRoomMember.java
@@ -1,13 +1,13 @@
 package com.api.meetudy.chat.entity;
 
 import com.api.meetudy.member.entity.Member;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Builder
 @Entity
-@ToString
 @Table(name  = "chat_room_member")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,25 +17,22 @@ public class ChatRoomMember {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String displayName;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chat_room_id", nullable = false)
     private ChatRoom chatRoom;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    public ChatRoomMember(ChatRoom chatRoom, Member member) {
-        this.chatRoom = chatRoom;
-        this.member = member;
-    }
-
-    public static ChatRoomMember of(String displayName, ChatRoom room, Member member) {
+    public static ChatRoomMember of(String displayName, ChatRoom chatRoom, Member member) {
         return ChatRoomMember.builder()
                 .displayName(displayName)
-                .chatRoom(room)
+                .chatRoom(chatRoom)
                 .member(member)
                 .build();
     }
