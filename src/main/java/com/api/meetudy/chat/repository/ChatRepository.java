ed5c04542701a2448aa41b0812a971ebd4d861e2
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-05-06 15:58:37 +0900

    [feature/#32] Add methods to find chats by room ID and latest chat by room

--- src/main/java/com/api/meetudy/chat/repository/ChatRepository.java
+++ src/main/java/com/api/meetudy/chat/repository/ChatRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-    List<Chat> findByRoom(ChatRoom room);
+    List<Chat> findByRoom_IdOrderByCreatedAtAsc(Long roomId);
+
+    Optional<Chat> findTopByRoomOrderByCreatedAtDesc(ChatRoom room);
 
 }
