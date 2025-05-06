a194c418bf761a56c1074ff1dfe558f99c5ad354
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-05-06 15:59:35 +0900

    [feature/#32] Add method to find all chat rooms by member

--- src/main/java/com/api/meetudy/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/api/meetudy/chat/repository/ChatRoomRepository.java
@@ -1,11 +1,15 @@
 package com.api.meetudy.chat.repository;
 
 import com.api.meetudy.chat.entity.ChatRoom;
+import com.api.meetudy.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
+    List<ChatRoom> findAllByMembers_Member(Member member);
 
 }
