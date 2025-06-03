dd72cdc6f59e54b743c4905f1f21755a7fd9931d
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-06-03 17:08:26 +0900

    [remove/#42] Remove unused methods after deleting comment retrieval API

--- src/main/java/com/api/meetudy/comment/mapper/CommentMapper.java
+++ src/main/java/com/api/meetudy/comment/mapper/CommentMapper.java
@@ -4,10 +4,7 @@ import com.api.meetudy.comment.dto.CommentDto;
 import com.api.meetudy.comment.dto.CommentRequestDto;
 import com.api.meetudy.comment.entity.Comment;
 import com.api.meetudy.member.entity.Member;
-import com.api.meetudy.post.entity.Post;
 import org.mapstruct.*;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = true))
 public interface CommentMapper {
@@ -19,11 +16,8 @@ public interface CommentMapper {
     Comment toComment(CommentRequestDto commentRequestDto, @Context Member author);
 
     @Mapping(target = "commentId", source = "id")
-    @Mapping(target = "authorNickName", source = "author.nickname")
+    @Mapping(target = "authorNickname", source = "author.nickname")
     CommentDto toCommentDto(Comment comment);
-
-    @IterableMapping(elementTargetType = CommentDto.class)
-    List<CommentDto> toCommentDtoList(List<Comment> comments);
 
     @AfterMapping
     default void setAuthor(@MappingTarget Comment comment, @Context Member author) {
--- src/main/java/com/api/meetudy/comment/repository/CommentRepository.java
+++ src/main/java/com/api/meetudy/comment/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package com.api.meetudy.comment.repository;
 
 import com.api.meetudy.comment.entity.Comment;
-import com.api.meetudy.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +8,5 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    List<Comment> findByPostOrderByCreatedAtAsc(Post post);
 
 }
