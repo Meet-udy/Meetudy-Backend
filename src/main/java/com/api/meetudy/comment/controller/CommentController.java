75478f54f63110952e698bc67aa1b5ced2d82216
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-06-03 17:05:47 +0900

    [remove/#42] Delete API for fetching comments of a specific post

--- src/main/java/com/api/meetudy/comment/controller/CommentController.java
+++ src/main/java/com/api/meetudy/comment/controller/CommentController.java
@@ -1,7 +1,6 @@
 package com.api.meetudy.comment.controller;
 
 import com.api.meetudy.auth.service.AuthenticationService;
-import com.api.meetudy.comment.dto.CommentDto;
 import com.api.meetudy.comment.dto.CommentRequestDto;
 import com.api.meetudy.comment.service.CommentService;
 import com.api.meetudy.global.response.ApiResponse;
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +21,8 @@ public class CommentController {
     private final AuthenticationService authenticationService;
 
     @Operation(summary = "댓글 작성 API")
-    @PostMapping
-    public ResponseEntity<ApiResponse<String>> addComment(Long postId,
+    @PostMapping("/{postId}")
+    public ResponseEntity<ApiResponse<String>> addComment(@PathVariable Long postId,
                                                           @Valid @RequestBody CommentRequestDto commentRequestDto,
                                                           Principal principal) {
         String response = commentService.addComment(postId, commentRequestDto, authenticationService.getCurrentMember(principal));
@@ -32,8 +30,8 @@ public class CommentController {
     }
 
     @Operation(summary = "댓글 수정 API")
-    @PutMapping
-    public ResponseEntity<ApiResponse<String>> updateComment(Long commentId,
+    @PutMapping("/{commentId}")
+    public ResponseEntity<ApiResponse<String>> updateComment(@PathVariable Long commentId,
                                                              @RequestBody CommentRequestDto commentRequestDto,
                                                              Principal principal) {
         String response = commentService.updateComment(commentId, commentRequestDto, authenticationService.getCurrentMember(principal));
@@ -41,16 +39,10 @@ public class CommentController {
     }
 
     @Operation(summary = "댓글 삭제 API")
-    @DeleteMapping
-    public ResponseEntity<ApiResponse<String>> deleteComment(Long commentId, Principal principal) {
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<ApiResponse<String>> deleteComment(@PathVariable Long commentId,
+                                                             Principal principal) {
         String response = commentService.deleteComment(commentId, authenticationService.getCurrentMember(principal));
-        return ResponseEntity.ok(ApiResponse.onSuccess(response));
-    }
-
-    @Operation(summary = "특정 게시물의 댓글 조회 API")
-    @GetMapping("/{postId}")
-    public ResponseEntity<ApiResponse<List<CommentDto>>> getCommentsByPost(@PathVariable Long postId) {
-        List<CommentDto> response = commentService.getCommentsByPost(postId);
         return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 
