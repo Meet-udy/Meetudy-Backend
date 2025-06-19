fe6bdf623927f5027e899afc416bced0a5182386
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-06-19 17:01:51 +0900

    [refactor/#48] Move study group package from study/group to group

---
+++ src/main/java/com/api/meetudy/group/dto/StudyGroupUpdateDto.java
@@ -0,0 +1,30 @@
+package com.api.meetudy.group.dto;
+
+import com.api.meetudy.group.enums.Location;
+import com.api.meetudy.group.enums.StudyCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class StudyGroupUpdateDto {
+
+    @Schema(description = "The name of the study group.")
+    private String name;
+
+    @Schema(description = "Description of the study group.")
+    private String description;
+
+    @Schema(description = "Maximum number of participants.")
+    private Integer maxParticipants;
+
+    @Schema(description = "Indicates whether the study is conducted online or offline.")
+    private Boolean isOnline;
+
+    @Schema(description = "Location of the study.")
+    private Location location;
+
+    @Schema(description = "The category or type of the study group.",
+            example = "LANGUAGE, CERTIFICATION")
+    private StudyCategory category;
+
+}
---
+++ src/main/java/com/api/meetudy/group/enums/StudyCategory.java
@@ -0,0 +1,38 @@
+package com.api.meetudy.group.enums;
+
+public enum StudyCategory {
+
+    // 어학
+    LANGUAGE,
+
+    // 자격증
+    CERTIFICATION,
+
+    // 인문학
+    HUMANITIES,
+
+    // 사회과학
+    SOCIAL_SCIENCES,
+
+    // 디자인
+    DESIGN,
+
+    // 과학
+    SCIENCE,
+
+    // 프로그래밍
+    PROGRAMMING,
+
+    // 취업/커리어
+    CAREER,
+
+    // 고시/공무원
+    QUALIFICATION_EXAM,
+
+    // 취미
+    HOBBY,
+
+    // 기타
+    OTHERS
+
+}
