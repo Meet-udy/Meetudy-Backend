416bf7c314985cc78f4ae95de3444716ee51d02a
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-06-28 14:16:48 +0900

    [feature/#54] Add query to fetch chat room with members by id

--- src/main/java/com/api/meetudy/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/api/meetudy/chat/repository/ChatRoomRepository.java
@@ -3,6 +3,8 @@ package com.api.meetudy.chat.repository;
 import com.api.meetudy.chat.entity.ChatRoom;
 import com.api.meetudy.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +16,8 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     List<ChatRoom> findAllByMembers_Member(Member member);
 
     Optional<ChatRoom> findByStudyGroup_Id(Long groupId);
+
+    @Query("SELECT r FROM ChatRoom r LEFT JOIN FETCH r.members m WHERE r.id = :id")
+    Optional<ChatRoom> findByIdWithMembers(@Param("id") Long id);
 
 }
